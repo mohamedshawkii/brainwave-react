1702be2a29af59ffa52ea98e5a7d671e53ff9f86
asdasdf <asdasffas@gmail.com>
2024-04-25 20:15:29 +0200

    Header + More Responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,17 @@ import { CardServices,CaseStudy,ChooseUs,Footer,Header,NavBar,Rates,Testimonial 
 function App() {
   return (
     <>
-    <div className="app">
-      <NavBar/>
-      <Header/>
-      <CardServices/>
-      <Rates/>
-      <ChooseUs/>
-      <CaseStudy/>
-      <Testimonial/>
-      <Footer/>
+      <div className='app'>
+        <div className=' h-svh bg-[url("/src/assets/BG-1.png")] bg-no-repeat' >
+          <NavBar/>
+          <Header/>
+        </div>
+        <CardServices/>
+        <Rates/>
+        <ChooseUs/>
+        <CaseStudy/>
+        <Testimonial/>
+        <Footer/>
       </div>
     </>
   )
