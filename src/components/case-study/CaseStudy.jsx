bde9240f65d3c76f4fb5ab0c57052d43f84267ec
asdasdf <asdasffas@gmail.com>
2024-06-02 17:14:02 +0300

    Commit

--- src/components/case-study/CaseStudy.jsx
+++ src/components/case-study/CaseStudy.jsx
@@ -9,8 +9,9 @@ import { motion } from "framer-motion";
 
 function CaseStudy() {
   return (
-    <div
-      className="flex flex-col
+    <section>
+      <div
+        className="flex flex-col
     justify-center items-center 
     gap-[4rem]
     py-[4rem]
@@ -18,85 +19,98 @@ function CaseStudy() {
     md:max-lg:px-[48px]
     lg:max-xl:px-[96px]
     xl:px-[224px]"
-    >
-      <div
-        className="
+      >
+        <div
+          className="
       py-4
       flex flex-col
       justify-center items-center gap-4"
-      >
-        <p className="font-base text-sm text-[#F64B4B]">Case studies</p>
-        <p className="font-bold text-2xl w-3/4 text-center">
-          Our works describe why we are the best in the business
-        </p>
+        >
+          <p className="font-base text-sm text-[#F64B4B]">Case studies</p>
+          <p className="font-bold text-2xl w-3/4 text-center">
+            Our works describe why we are the best in the business
+          </p>
+        </div>
+        <div className=" grid grid-cols-2 grid-rows-1 xtsm:max-md:w-4/6 xtsm:max-md:grid-cols-1 gap-16">
+          <div className="row-span-2">
+            <motion.img
+              src={ImgeTwo}
+              alt=""
+              initial={{ opacity: 0, x: 40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1.5, ease: "easeIn" }}
+            />
+            <motion.div
+              className=" ml-8 my-4"
+              initial={{ opacity: 0, x: 40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 0.2, duration: 1.5, ease: "easeIn" }}
+            >
+              <p>Graphic Design</p>
+              <p className="font-bold text-lg">AB.S Snack Packaging</p>
+            </motion.div>
+          </div>
+          <div className="">
+            <motion.img
+              src={ImgeOne}
+              alt=""
+              initial={{ opacity: 0, x: 40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1.5, ease: "easeIn" }}
+            />
+            <motion.div
+              className=" ml-8 my-4"
+              initial={{ opacity: 0, x: 40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 0.2, duration: 1.5, ease: "easeIn" }}
+            >
+              <p>Graphic Design</p>
+              <p className="font-bold text-lg">Aura Branding Design</p>
+            </motion.div>
+          </div>
+          <div className="row-span-2">
+            <motion.img
+              src={ImgeThree}
+              alt=""
+              initial={{ opacity: 0, x: 40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1.5, ease: "easeIn" }}
+            />
+            <motion.div
+              className=" ml-8 my-4"
+              initial={{ opacity: 0, x: 40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 0.2, duration: 1.5, ease: "easeIn" }}
+            >
+              <p>Web Development</p>
+              <p className="font-bold text-lg">Gradient Website Development</p>
+            </motion.div>
+          </div>
+          <div className="">
+            <motion.img
+              src={ImgeFour}
+              alt=""
+              initial={{ opacity: 0, x: 40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 1.5, ease: "easeIn" }}
+            />
+            <motion.div
+              className="ml-8 my-4"
+              initial={{ opacity: 0, x: 40 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ delay: 0.2, duration: 1.5, ease: "easeIn" }}
+            >
+              <p>Content Writing</p>
+              <p className="font-bold text-lg">Magazine Content Writing</p>
+            </motion.div>
+          </div>
+        </div>
+        <SeeAllButton
+          title={"See all works"}
+          Icon={<MdOutlineArrowForward size={27} />}
+        />
       </div>
-      <div className=" grid grid-cols-2 grid-rows-1 xtsm:max-md:w-4/6 xtsm:max-md:grid-cols-1 gap-16">
-        <div className="row-span-2">
-          <motion.img src={ImgeTwo} alt="" 
-          initial={{ opacity: 0, x: 40 }}
-          whileInView={{ opacity: 1,  x:0}}
-          transition={{ duration: 1.5, ease:"easeIn" }}
-          />
-          <motion.div className=" ml-8 my-4"
-          initial={{ opacity: 0, x: 40 }}
-          whileInView={{ opacity: 1,  x:0}}
-          transition={{ delay: 0.20, duration: 1.5, ease:"easeIn" }}
-          >
-            <p>Graphic Design</p>
-            <p className="font-bold text-lg">AB.S Snack Packaging</p>
-          </motion.div>
-        </div>
-        <div className="">
-          <motion.img src={ImgeOne} alt="" 
-          initial={{ opacity: 0, x: 40 }}
-          whileInView={{ opacity: 1,  x:0}}
-          transition={{ duration: 1.5, ease:"easeIn" }}
-          />
-          <motion.div className=" ml-8 my-4"
-          initial={{ opacity: 0, x: 40 }}
-          whileInView={{ opacity: 1,  x:0}}
-          transition={{ delay: 0.20, duration: 1.5, ease:"easeIn" }}
-          >
-            <p>Graphic Design</p>
-            <p className="font-bold text-lg">Aura Branding Design</p>
-          </motion.div>
-        </div>
-        <div className="row-span-2">
-          <motion.img src={ImgeThree} alt="" 
-          initial={{ opacity: 0, x: 40 }}
-          whileInView={{ opacity: 1,  x:0}}
-          transition={{ duration: 1.5, ease:"easeIn" }}
-          />
-          <motion.div className=" ml-8 my-4"
-          initial={{ opacity: 0, x: 40 }}
-          whileInView={{ opacity: 1,  x:0}}
-          transition={{ delay: 0.20, duration: 1.5, ease:"easeIn" }}
-          >
-            <p>Web Development</p>
-            <p className="font-bold text-lg">Gradient Website Development</p>
-          </motion.div>
-        </div>
-        <div className="">
-          <motion.img src={ImgeFour} alt="" 
-          initial={{ opacity: 0, x: 40 }}
-          whileInView={{ opacity: 1,  x:0}}
-          transition={{ duration: 1.5, ease:"easeIn" }}
-          />
-          <motion.div className="ml-8 my-4"
-          initial={{ opacity: 0, x: 40 }}
-          whileInView={{ opacity: 1,  x:0}}
-          transition={{ delay: 0.20, duration: 1.5, ease:"easeIn" }}
-          >
-            <p>Content Writing</p>
-            <p className="font-bold text-lg">Magazine Content Writing</p>
-          </motion.div>
-        </div>
-      </div>
-      <SeeAllButton
-        title={"See all works"}
-        Icon={<MdOutlineArrowForward size={27} />}
-      />
-    </div>
+    </section>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,8 +4,9 @@ import { motion } from "framer-motion";
 
 function Header() {
   return (
-    <div
-      className="
+    <header>
+      <div
+        className="
           py-8
           flex flex-row 
           justify-center items-center 
@@ -16,9 +17,9 @@ function Header() {
           md:max-lg:px-[48px]
           lg:max-xl:px-[96px]
           xl:px-[224px]"
-    >
-      <div
-        className="
+      >
+        <div
+          className="
         flex 
         flex-col
         justify-start 
@@ -26,53 +27,54 @@ function Header() {
         gap-4 
         w-5/12
         xtsm:max-sm:w-full"
-      >
-        <motion.p
-          className=" text-[#F64B4B] font-normal text-md"
-          initial={{ opacity: 0, x: -100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
         >
-          Let’s shift your business
-        </motion.p>
-        <motion.p
-          className=" font-black xtsm:max-sm:text-xl text-4xl"
-          initial={{ opacity: 0, x: -120 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
-        >
-          Shift your business fast with Shade Pro.
-        </motion.p>
-        <motion.p
-          className=" font-normal text-base"
-          initial={{ opacity: 0, x: -130 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
-        >
-          With lots of unique blocks, you can easily build a page without
-          coding. Build your next consultancy website within few minutes.
-        </motion.p>
+          <motion.p
+            className=" text-[#F64B4B] font-normal text-md"
+            initial={{ opacity: 0, x: -100 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
+          >
+            Let’s shift your business
+          </motion.p>
+          <motion.p
+            className=" font-black xtsm:max-sm:text-xl text-4xl"
+            initial={{ opacity: 0, x: -120 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
+          >
+            Shift your business fast with Shade Pro.
+          </motion.p>
+          <motion.p
+            className=" font-normal text-base"
+            initial={{ opacity: 0, x: -130 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
+          >
+            With lots of unique blocks, you can easily build a page without
+            coding. Build your next consultancy website within few minutes.
+          </motion.p>
+          <motion.div
+            className=" xtsm:max-sm:hidden"
+            initial={{ opacity: 0, x: -135 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
+          >
+            <CTAOnLightBg />
+          </motion.div>
+        </div>
         <motion.div
-          className=" xtsm:max-sm:hidden"
-          initial={{ opacity: 0, x: -135 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
-        >
-          <CTAOnLightBg />
-        </motion.div>
-      </div>
-      <motion.div
-        className="
+          className="
           xtsm:max-sm:w-7/12  
           sm:max-2xl:w-2/6
-          2xl:max-wideScreen:w-1/6  "
-        initial={{ opacity: 0, x: 120 }}
-        animate={{ opacity: 1, x: 0 }}
-        transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
-      >
-        <img src={SideImg} alt="" />
-      </motion.div>
-    </div>
+          2xl:w-2/6 "
+          initial={{ opacity: 0, x: 120 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1, eae: "easeOut", delay: 0.2 }}
+        >
+          <img src={SideImg} alt="" />
+        </motion.div>
+      </div>
+    </header>
   );
 }
 
